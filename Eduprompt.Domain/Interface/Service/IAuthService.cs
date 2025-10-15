3504e4831634894ec55b38ab96cb2df58e3db5b2
Lê Trọng Hiếu <tronghieu848@gmail.com>
2025-10-15 23:06:50 +0700

    update

--- Eduprompt.Domain/Interface/Service/IAuthService.cs
+++ Eduprompt.Domain/Interface/Service/IAuthService.cs
@@ -5,5 +5,5 @@ namespace Eduprompt.Domain.Interface.Service;
 public interface IAuthService
 {
     Task<AuthResponseDto> RegisterAsync(RegisterRequestDto request);
-    Task<AuthResponseDto> LoginAsync(LoginRequestDto request);
+    Task<TokenResponseDto> LoginAsync(LoginRequestDto request);
 } 
